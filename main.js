755cd1084a4ba7e5bd29ba77632e31f45a4d10ae
unknown <rian.mizar04@gmail.com>
2024-12-05 16:07:17 +0700

    delete code main.js

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
-(function() {
-    emailjs.init("YOUR_USER_ID"); // Ganti dengan User ID dari EmailJS
-})();
-
-// Tambahkan event listener ke form
-document.getElementById('emailForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Mencegah refresh halaman
-
-    // Kirim form menggunakan EmailJS
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this)
-        .then(function() {
-            alert("Email berhasil dikirim!");
-        }, function(error) {
-            alert("Gagal mengirim email: " + JSON.stringify(error));
-        });
-});
-
-// Ambil elemen button
 const scrollToTopBtn = document.getElementById('scrollToTop');
 
-// Tampilkan tombol ketika scroll ke bawah
 window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
         scrollToTopBtn.classList.add('show');
@@ -27,10 +8,9 @@ window.addEventListener('scroll', () => {
     }
 });
 
-// Scroll ke atas saat tombol diklik
 scrollToTopBtn.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
-        behavior: 'smooth' // Animasi scroll
+        behavior: 'smooth' 
     });
 });
