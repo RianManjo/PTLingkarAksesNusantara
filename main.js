bc0b6fa517497d0d3f9569f72f7ae22e97d3716c
unknown <rian.mizar04@gmail.com>
2024-12-07 15:15:35 +0700

    add all files fix

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 const scrollToTopBtn = document.getElementById('scrollToTop');
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
+const currentYear = new Date().getFullYear();
 
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {    
     if (window.scrollY > 300) {
         scrollToTopBtn.classList.add('show');
     } else {
         scrollToTopBtn.classList.remove('show');
     }
+
+    let current = "";
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(current)) {
+            link.classList.add("active");
+        }
+    });
 });
 
 scrollToTopBtn.addEventListener('click', () => {
@@ -14,3 +34,5 @@ scrollToTopBtn.addEventListener('click', () => {
         behavior: 'smooth' 
     });
 });
+
+document.getElementById('currentYear').textContent = currentYear;
